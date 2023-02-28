77a97dfecb617abe689e95505b49e1aabf598937
shard7 <106669955+shard77@users.noreply.github.com>
2023-02-28 09:10:14 +0100

    added sysinfo command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod commands;
 
 use std::io::{stdin, stdout, Write};
 use std::fs;
-
+use std::env;
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
 fn main() {
     loop {
@@ -43,6 +44,33 @@ fn main() {
                 if !params_vector.is_empty() {
                     commands::cd(&params_vector[0]);
                 }
+            }
+
+            Some("sysinfo") => {
+                let mut sys = System::new_all();
+                sys.refresh_all();
+                println!("Disks:");
+                for disk in sys.disks() {
+                    println!("{:?}", disk);
+                }
+                println!("=> networks:");
+                for (interface_name, data) in sys.networks() {
+                    println!("{}: {}/{} B", interface_name, data.received(), data.transmitted());
+                }
+                println!("=> components:");
+                for component in sys.components() {
+                    println!("{:?}", component);
+                }
+                println!("=> system:");
+                println!("total memory: {} bytes", sys.total_memory());
+                println!("used memory : {} bytes", sys.used_memory());
+                println!("total swap  : {} bytes", sys.total_swap());
+                println!("used swap   : {} bytes", sys.used_swap());
+                println!("System name:             {:?}", sys.name());
+                println!("System kernel version:   {:?}", sys.kernel_version());
+                println!("System OS version:       {:?}", sys.os_version());
+                println!("System host name:        {:?}", sys.host_name());
+                println!("NB CPUs: {}", sys.cpus().len());
             }
 
             _ => println!("command not known")
